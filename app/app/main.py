b0f69d16c1d469ca9e70b117a60e3475aa900103
ryabukha <s.ryabukha@gmail.com>
2020-10-13 17:26:44 +0300

    try to add func

--- app/app/main.py
+++ app/app/main.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 from . import db
+from .models import Response
 from flask_login import login_required, current_user
 
 main = Blueprint('main', __name__)
@@ -12,3 +13,17 @@ def index():
 @login_required
 def profile():
     return render_template('profile.html', name=current_user.name)
+
+# @main.route('/response_form')
+# def response_form():
+#     print('response func, ' + current_user.name)
+#     return render_template('response_form.html')
+
+@main.route('/response_form')
+@login_required
+def response_form():
+    new_message = Response(email='b@b.b', title='bname', message='hello')
+    db.session.add(new_message)
+    db.session.commit()
+    print('response func, ' + current_user.name)
+    return render_template('response_form.html')
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 app="docker.auth"
 docker build -t ${app} .
-docker run -it -p 80:80 --name=${app} -v $PWD/app:/app ${app}
+docker run -it --rm -p 80:80 --name=${app} -v $PWD/app:/app ${app}
