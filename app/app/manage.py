c9f66aca1a22f92613199ecfdf42f03c3ce47a0e
ryabukha <s.ryabukha@gmail.com>
2020-12-08 16:48:14 +0200

    xm

--- app/app/manage.py
+++ app/app/manage.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from models import *
+# from models import *
 
 app.config['SECRET_KEY'] = 'secret-key-goes-here'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
@@ -13,11 +13,21 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(128))
-#     role = db.Column(db.String(128))
-#     lastName = db.Column(db.String(128))
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
+    role_id = db.Column(db.Integer)
 
+class Response(db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    email = db.Column(db.String(100))
+    subject = db.Column(db.String(100))
+    message = db.Column(db.String(1000))
+
+class Role(db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    name = db.Column(db.String(100))
 if __name__ == '__main__':
     manager.run()
