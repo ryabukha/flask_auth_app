fd4bd651c9d5e47182806898fdc8e926d064af9a
ryabukha <s.ryabukha@gmail.com>
2020-12-10 17:22:10 +0200

    add Class to manage

--- app/manage.py
+++ app/manage.py
@@ -1,8 +1,9 @@
 from flask import Flask
+from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from app.models import *
+# from app.models import *
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret-key-goes-here'
@@ -13,6 +14,23 @@ migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
+    role_id = db.Column(db.Integer)
+
+class Response(db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    email = db.Column(db.String(100))
+    subject = db.Column(db.String(100))
+    message = db.Column(db.String(1000))
+
+class Role(db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    name = db.Column(db.String(100))
 
 if __name__ == '__main__':
     manager.run()
